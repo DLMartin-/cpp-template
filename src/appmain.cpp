8659fbb238fc7e6c8be652a967534348e23243e5
Daniel Martin <ashinyacorn@gmail.com>
2023-03-13 23:26:16 -0400

    Add the fmt library for pretty printing

--- src/appmain.cpp
+++ src/appmain.cpp
@@ -1,18 +1,25 @@
 #include <SDL.h>
+#include <fmt/color.h>
+#include <fmt/core.h>
 
-[[nodiscard]] int app_main() noexcept { 
-  SDL_Window* window = SDL_CreateWindow("My App", 480, 300, 0);
-  SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr, 0);
+[[nodiscard]] int app_main() noexcept {
+  SDL_Window *window = SDL_CreateWindow("My App", 480, 300, 0);
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, nullptr, 0);
 
-  if(window == nullptr || renderer == nullptr) {
+  if (window == nullptr || renderer == nullptr) {
+    fmt::print(fg(fmt::color::crimson) | fmt::emphasis::bold,
+               "There has been an error creating a window or renderer!\n");
+    fmt::print(fg(fmt::color::crimson) | fmt::emphasis::bold, "Error: {}",
+               SDL_GetError());
     return -1;
   }
 
   SDL_SetRenderDrawColor(renderer, 255, 0, 255, SDL_ALPHA_OPAQUE);
   SDL_Event event;
-  while(1) {
-    while(SDL_PollEvent(&event)) {
-      if(event.type == SDL_EVENT_QUIT) return 0;
+  while (1) {
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_EVENT_QUIT)
+        return 0;
     }
 
     SDL_RenderClear(renderer);
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
-#include <SDL3/SDL.h>
 #include "appmain.h"
+#include <SDL3/SDL.h>
+#include <fmt/color.h>
+#include <fmt/core.h>
 
-int main (int const argc, char const * const argv[])
-{
-  if(SDL_Init(SDL_INIT_EVERYTHING) != 0) return -1;
+int main(int const argc, char const *const argv[]) {
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+    fmt::print(fg(fmt::color::crimson) | fmt::emphasis::bold,
+               "There has been an error while initializing SDL!");
+    fmt::print(fg(fmt::color::crimson) | fmt::emphasis::bold, "Error: {}",
+               SDL_GetError());
+    return -1;
+  }
 
   return app_main();
 }
-
